04858c1678a31ef753c2a530fd7a8f7c917c01df
tMasaaa <noumodulity@gmail.com>
2019-07-07 06:16:17 +0000

    add

--- part2/dprintf/main.c
+++ part2/dprintf/main.c
@@ -1,1 +1,21 @@
-HELLO main.c!
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+    int fd;
+
+    if (argc != 2){
+        fprintf(stderr, "filename required\n");
+        exit(1);
+    }
+    fd = open(argv[1], O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd < 0) {
+        fprintf(stderr, "file descripter can't open\n");
+        exit(1);
+    }
+    dprintf(fd, "HELLO %s!\n", argv[1]);
+    close(fd);
+    return 0;
+}
