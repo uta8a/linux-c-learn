388bca20b77ff3053d6449ef95a53951e17ec67e
tMasaaa <noumodulity@gmail.com>
2019-07-07 04:23:11 +0000

    add

--- part2/b/main.c
+++ part2/b/main.c
@@ -2,8 +2,16 @@
 int main(int argc, char **argv) {
     char *p = 0;
     int top = 100;
-    printf("&p    = 0x%lx\n", (unsigned long)&p);
-    printf("&top  = 0x%lx\n", (unsigned long)&top);
-    printf("&argc = 0x%lx\n", (unsigned long)&argc);
-    printf("&argv = 0x%lx\n", (unsigned long)&argv);
+    int last = 10000000;
+    int llast = 100000;
+    int a[10] = {1,2,3,4,5,6,7,8,9,10};
+    int b[10] = {1,2,3,4,5,6,7,8,9,10};
+    printf("&p    = %p\n", &p);
+    printf("&top  = %p\n", &top);
+    printf("&argc = %p\n", &argc);
+    printf("&argv = %p\n", &argv);
+    printf("&last = %p\n", &last);
+    printf("&llast= %p\n", &llast);
+    printf("&a    = %p\n", &a);
+    printf("&b    = %p\n", &b);
 }
---
+++ part2/b/num1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main() {
+    int a=1;
+    int b=2;
+    printf("%d", (unsigned long)&a);
+}
